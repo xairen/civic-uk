04bb31108e8fabd9dfc785f2e12f7829f44f8a34
Abhinav Srinivasan <92839740+xairen@users.noreply.github.com>
2024-02-28 16:49:16 -0500

    initial setup - uk

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import "./plugins/bootstrap-vue";
+import "./plugins/axios";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount("#app");
